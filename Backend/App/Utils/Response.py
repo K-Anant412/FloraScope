a907c17ed6f5b7eac8324832407e73533fd23966
K-Anant412 <anantkore412@gmail.com>
2026-08-16 13:30:44 +0530

    creating a utility response structure to handle user login, and store JWT access token.

--- Backend/App/Utils/Response.py
+++ Backend/App/Utils/Response.py
@@ -11,3 +11,11 @@ def error_response(message, status_code=400):
         "success": False,
         "data": None
     }), status_code
+    
+def auth_response(message, data=None, access=None ,status_code=200):
+    return ({
+        "message": message,
+        "success": True,
+        "data": data,
+        "access_token": access
+    }), status_code
