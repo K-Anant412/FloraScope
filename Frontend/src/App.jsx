87be767aa30153cf3366c98667747e2921bb8c43
K-Anant412 <anantkore412@gmail.com>
2026-08-23 20:28:33 +0530

    creating login UI.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,12 @@ import './App.css'
 
 function App() {
 
+  //  background color: #E8F5E9
+  //  Primary color: #4F9D4D
+  //  Secondary color: #A8D58D
+  //  Accent color: #F8E7A2
+  //  Dark color: #285943
+
   return (
     <>
       <section className="relative w-screen h-screen overflow-hidden">
@@ -32,8 +38,29 @@ function App() {
         <div className="relative z-30 w-full h-full overflow-hidden flex items-center justify-start md:pl-20 p-5">
 
           {/* Login card */}
-          <div className="border rounded-2xl md:w-[40%] w-full md:h-[80%] h-[60%] relative md:top-0 border-gray-400 -top-28 shadow-[6px_8px_20px_rgba(0,0,0,0.22),-8px_-8px_20px_rgba(255,255,255,0.12)]">
-            {/* Your login form */}
+          <div className="border rounded-2xl md:w-[35%] w-full md:h-[80%] h-[60%] relative md:-top-10 md:left-40 border-gray-400 -top-28 shadow-[6px_8px_20px_rgba(0,0,0,0.22),-8px_-8px_20px_rgba(255,255,255,0.12)] flex flex-col items-center md:p-5 p-3 bg-[#E8F5E9]">
+            
+            <h1 className='text-[26px] text-[#285943] font-semibold font-["Fredoka"] h-fit p-3 w-full flex items-center justify-center pt-6'>
+              Login for Identify
+            </h1>
+
+
+            <div className='w-full h-fit p-3 flex flex-col items-center justify-center'>
+
+              {/* <label> email </label> */}
+              <input type="text" placeholder='enter your email' className=' m-3 border w-[90%] md:w-[70%] h-fit p-2 md:p-3 placeholder:font-["Nunito"] font-semibold rounded-2xl border-gray-500' />
+              <input type="password" placeholder='password' className=' m-3 mt-2 border w-[90%] md:w-[70%] h-fit p-2 md:p-3 placeholder:font-["Nunito"] font-semibold rounded-2xl border-gray-500' />
+
+              <button className='border transition-all duration-300 bg-[#4F9D4D] hover:-translate-y-0.5 hover:bg-[#3d763b] text-white font-["Fredoka"] text-2xl font-semibold p-1 mt-6 w-[90%] h-fit rounded-[10px] border-gray-500' >
+                login
+              </button>
+            </div>
+
+            <p className='text-sm flex gap-1'>
+              Dont have an account? 
+              <a href="#" className='font-semibold text-[#4F9D4D] transition-all duration-200 cursor-pointer hover:text-[#41803f]'>create here</a>
+            </p>
+
           </div>
 
         </div>
