cd2f2d171c2e5c4eb6cb138774d76647c0576242
K-Anant412 <anantkore412@gmail.com>
2026-08-25 14:59:10 +0530

    completing the login card UI.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import desktop_bg_sticker from '../public/Desktop_image/desktop_bg_sticker.png';
 
 import phone_bg from '../public/Phone_image/phone_bg.jpg';
 import phone_bg_sticker from '../public/Phone_image/phone_bg_sticker.png';
+import AuthPage from './pages/AuthPage';
 import './App.css'
 
 function App() {
@@ -34,28 +35,8 @@ function App() {
           }}
         />
 
-        {/* Login / Registration */}
-        <div className="relative z-30 w-full h-full overflow-hidden flex items-center justify-center md:pl-20 p-5">
-
-          {/* Login card */}
-          <div className="border rounded-2xl md:w-[65%] w-full md:h-[65%] h-[60%] relative md:-top-10 border-gray-400 -top-28 shadow-[6px_8px_20px_rgba(0,0,0,0.22),-8px_-8px_20px_rgba(255,255,255,0.12)] flex items-center md:p-0 p-3 bg-[#E8F5E9]">
-            {/*  Input fields  */}
-            <div className='md:w-[50%] w-full h-full md:border-r-2 border-gray-400 flex flex-col items-center'>
-
-              <h1>FloraScope</h1>
-
-              
-
-            </div>
-
-            {/*  Open section, visible only for desktop  */}
-            <div className='md:flex hidden '>
-
-            </div>
-
-          </div>
-
-        </div>
+          <AuthPage />
+        
 
         {/* Desktop foreground */}
         <div
---
+++ Frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const AuthPage = () => {
+  return (
+    <div className="relative z-30 w-full h-full overflow-hidden flex items-center justify-center md:pl-20 p-5">
+
+          {/* Login card */}
+          <div className="border rounded-2xl md:w-[65%] w-full md:h-[65%] h-[60%] relative md:-top-10 border-white/40 -top-28 shadow-[6px_8px_20px_rgba(0,0,0,0.22),-8px_-8px_20px_rgba(255,255,255,0.12)] flex items-center md:p-0 p-3 bg-[#E8F5E9]">
+            {/*  Input fields  */}
+            <div className='md:w-[50%] w-full h-full flex flex-col items-center justify-center p-4'>
+              <div className='w-full h-full flex flex-col items-center border-2 rounded-2xl bg-[#5fb25cc3] md:shadow-gray-600 md:shadow-[inset_0_0_8px_2px_rgba(0,0,0,0.06)]'>
+
+              
+              <h1 className='shrink-0 w-full text-[#E8F5E9] h-fit font-["Fredoka"] text-2xl md:text-4xl font-semibold flex items-center justify-center p-3 md:p-6'>
+                FloraScope
+              </h1>
+
+              <div className='md:border-2 w-full h-full md:w-[80%] md:h-[70%] md:bg-[#E8F5E9] rounded-2xl border-white/40 shadow-[6px_8px_20px_rgba(0,0,0,0.22),-8px_-8px_20px_rgba(255,255,255,0.12)] flex flex-col items-center'>
+                
+                <h1 className='w-full h-fit text-gray-600 p-1 md:p-0 md:mt-6 md:text-2xl text-xl font-["nunito"] font-bold flex items-center justify-center'>
+                  Login To Identify
+                </h1>
+
+                <div className='w-full h-fit m-4 flex flex-col items-center justify-center'>
+
+                  <input 
+                    type="text" 
+                    placeholder='@email'  
+                    className='w-[90%] md:w-[80%] h-10 m-2 bg-[#E8F5E9] border-2 rounded-3xl border-white md:border-gray-400 placeholder:text-xl pl-3 outline-none focus:border-[#A8D58D] focus:ring-2 focus:ring-[#A8D58D]'  
+                    />
+                  <input 
+                    type="password" 
+                    placeholder='password'  
+                    className='w-[90%] md:w-[80%] h-10 m-2 bg-[#E8F5E9] border-2 rounded-3xl border-white md:border-gray-400 placeholder:text-xl pl-3 outline-none focus:border-[#A8D58D] focus:ring-2 focus:ring-[#A8D58D]'  
+                    />
+
+                </div>
+
+                <button
+                  className='border rounded-3xl w-[60%] h-fit p-2 shadow-xl font-bold text-xl flex items-center justify-center bg-[#4F9D4D] text-white'
+                >
+                  Log-in
+                </button>
+
+                <p className='w-full h-fit flex items-center justify-center gap-1 mt-3'>
+                  Need an account?
+                  <a href="#"
+                  className='text-[16px] font-semibold font-["nunito] text-[#377135] transition-all duration-400 hover:underline'
+                  >
+                  Sign up
+                  </a>
+                </p>
+              </div>
+            </div>
+            </div>
+
+            {/*  Open section, visible only for desktop  */}
+            <div className='md:flex hidden '>
+
+            </div>
+
+          </div>
+
+    </div>
+  )
+}
+
+export default AuthPage
