493fb08fa21f1164d3986cf3dfbc50bb731f0a23
K-Anant412 <anantkore412@gmail.com>
2026-09-06 22:32:21 +0530

    some UI changes on User card Phone view.

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -15,20 +15,20 @@ const Profile = ({setPlantHistory}) => {
   
 
   return (
-    <section className='w-full h-full flex items-center justify-center md:p-6 p-3 pt-0'>
+    <section className='border-4 w-full h-full flex items-center justify-center md:p-6 pt-0 bg-cover bg-center'>
 
-      <div className='relative w-full h-full border-2 overflow-hidden rounded-3xl border-black flex flex-col'>
+      <div className='relative w-full h-full overflow-hidden md:rounded-3xl flex flex-col'>
 
-        <div className='w-full h-[28%] border-b-2 bg-white absolute'></div>
+        <div className='w-full md:h-[28%] h-40 border-b-2 bg-center bg-cover absolute' style={{backgroundImage: "url('/Desktop_image/profile.jpg')"}}></div>
 
         {/*  Profile header---> Card, Graph */}
-        <div className='shrink-0 w-full h-[50%] flex flex-col z-50 border border-black p-0 md:p-5'>
+        <div className='shrink-0 w-full h-[50%] flex flex-col z-50 p-0 md:p-5'>
 
-          <div className='w-[13%] h-45 rounded-[50%] border-4 relative top-5 left-5 border-[#E8F5E9] bg-white'>
+          <div className='md:w-[13%] w-30 md:h-45 h-30 rounded-[50%] border-4 relative md:top-15 top-26 left-5 border-[#E8F5E9] bg-white'>
 
           </div>
 
-          <h1 className='text-2xl z-50 relative top-5 font-semibold'>User Name</h1>
+          <h1 className='text-2xl z-50 relative md:top-15 top-26 md:left-10 left-5 font-semibold'>User Name</h1>
 
         </div>
 
