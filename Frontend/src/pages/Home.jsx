f8e5093e36d15e504cae226022bb690868f25244
K-Anant412 <anantkore412@gmail.com>
2026-09-13 22:14:43 +0530

    some changes.

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -82,7 +82,7 @@ const Home = () => {
             
             <div className='w-full min-h-screen bg-[#E8F5E9] flex flex-col p-10 gap-4'>
 
-              <div className='shrink-0 p-4 w-full md:h-85 border-2 rounded-3xl bg-[#A8D58D] flex flex-col gap-3'>
+              <div className='shrink-0 p-4 w-full md:h-85 border-2 rounded-3xl border-[#A8D58D] flex flex-col gap-3'>
 
                 <h1 className='shrink-0 w-full flex flex-col text-3xl font-["nunito"] font-bold text-[#2b532a]'>
                  Why Plants Matter?
@@ -97,14 +97,14 @@ const Home = () => {
 
               </div>
 
-              <div className='shrink-0 p-4 px-50 w-full md:h-60 border-2 rounded-3xl bg-[#A8D58D] flex items-center gap-50'>
+              <div className='shrink-0 p-4 px-50 w-full md:h-60  rounded-3xl bg-[#A8D58D] flex items-center gap-50'>
 
-                <h1 className='shrink-0 w-[30%] flex flex-col h-full border-2 justify-center text-4xl font-["nunito"] font-bold text-[#2b532a]'>
+                <h1 className='shrink-0 w-[30%] flex flex-col h-full justify-center text-4xl font-["nunito"] font-bold text-[#2b532a]'>
                  Ready to discover your plant?
                   <p className='text-xl font-normal flex gap-1'>Join thousands of plant lovers and make your green journey easier</p>
                 </h1>
 
-                <button className='text-2xl font-bold font-["nunito"] w-fit h-fit py-2 px-6 border-2 rounded-4xl'>
+                <button className='text-2xl font-bold font-["nunito"] w-fit h-fit py-2 px-6 border-2 rounded-4xl transition-all duration-300 hover:text-gray-700 hover:bg-[#E8F5E9] '>
                   Identify Now
                 </button>
 
