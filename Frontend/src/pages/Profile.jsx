a00e2b94260c59c20b3d50d763f4bba4643c6f29
K-Anant412 <anantkore412@gmail.com>
2026-09-19 19:46:30 +0530

    feat(profile): build profile header banner and user details section

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -1,80 +1,98 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { plantService } from '../service/api';
 import { AuthContext } from '../context/AuthContext';
-import ProfileImg from '../assets/Desktop_image/profile.jpg'
-import { MdAlternateEmail } from "react-icons/md";
-import { TbLogout } from "react-icons/tb";
+
+// icons
+import { IoMailOpenOutline } from "react-icons/io5";
+import { IoCalendarClearOutline } from "react-icons/io5";
+
+// images
+import camera from '../assets/Desktop_image/camera.png'
+import profile from '../assets/Desktop_image/profile_image.jpg'
+import banner from '../assets/Desktop_image/profile_banner.png'
 
 const Profile = () => {
   
-  const {user} =  useContext(AuthContext);
-  const [userName, setUserName] = useState('');
-  const [userEmail, setUserEmail] = useState('');
+  // const {user} =  useContext(AuthContext);
+  // const [userName, setUserName] = useState('');
+  // const [userEmail, setUserEmail] = useState('');
 
-  const [plantHistory, setPlantHistory] = useState([]);
-  const [fetchingData, setFetchingData] = useState(true);
-  const [requestError, setRequestError] = useState(null);
+  // const [plantHistory, setPlantHistory] = useState([]);
+  // const [fetchingData, setFetchingData] = useState(true);
+  // const [requestError, setRequestError] = useState(null);
 
-  useEffect(() => {
-    console.log("Current user state in Profile:", user);
-    if (!user) {
-    console.log("fetch_plant did not run because user is null/falsy");
-    return;
-  }
-    const fetch_plant = async() =>{
-      try {
-        setFetchingData(true);
-        setRequestError(null);
+  // useEffect(() => {
+  //   console.log("Current user state in Profile:", user);
+  //   if (!user) {
+  //   console.log("fetch_plant did not run because user is null/falsy");
+  //   return;
+  // }
+  //   const fetch_plant = async() =>{
+  //     try {
+  //       setFetchingData(true);
+  //       setRequestError(null);
 
-        const response = await plantService.plantHistory();
+  //       const response = await plantService.plantHistory();
 
-        setPlantHistory(response.data.data || []);
-        console.log(response.data.data);
+  //       setPlantHistory(response.data.data || []);
+  //       console.log(response.data.data);
       
-      } catch (error) {
-        if (error.response?.status ===400 && error.response?.data?.message ==="Empty dataset."){
-          setPlantHistory([]);
-        }
-      } finally {
-        setFetchingData(false)
-      }
-    };
+  //     } catch (error) {
+  //       if (error.response?.status ===400 && error.response?.data?.message ==="Empty dataset."){
+  //         setPlantHistory([]);
+  //       }
+  //     } finally {
+  //       setFetchingData(false)
+  //     }
+  //   };
 
-    if(user) {
-      setUserName(user.name)
-      setUserEmail(user.email)
-      fetch_plant();
-    }
+  //   if(user) {
+  //     setUserName(user.name)
+  //     setUserEmail(user.email)
+  //     fetch_plant();
+  //   }
 
-  }, [user])
+  // }, [user])
   
 
   return (
-    <section className='border-4 w-full h-full flex items-center justify-center md:p-6 pt-0 bg-cover bg-center'>
+    <section className='w-full h-full flex items-center justify-center md:p-6 bg-[#E8F5E9]'>
 
-      <div className='relative w-full h-full overflow-hidden md:rounded-3xl flex flex-col'>
+      <div className='w-full h-full border border-black rounded-3xl flex flex-col items-center overflow-hidden'>
 
-        <div className='w-full md:h-[28%] h-40 border-b-2 bg-center bg-cover absolute bg-white overflow-hidden'>
-          <img src={ProfileImg} alt="desktop Profile" className='w-full h-full cover' />
-        </div>
+        {/*  Header section for profile image, name, edit option */}
+        <div className='w-full h-[35%] flex items-center md:pl-10 md:p-4 gap-5 bg-center bg-cover' style={{backgroundImage: `url(${banner})`}}>
 
-        {/*  Profile header---> User image, name */}
-        <div className='shrink-0 w-full h-fit md:h-[50%] flex items-center md:gap-10 z-50 p-3 md:p-5 relative md:top-0 py-4.5 gap-3'>
+          {/*  profile image  */}
+          <div className='h-55 w-55 shrink-0 rounded-[50%] relative bg-center bg-cover border-white/40 shadow-[6px_8px_20px_rgba(0,0,0,0.22),-8px_-8px_20px_rgba(255,255,255,0.12)]' style={{backgroundImage:`url(${profile})`}}>
 
-          <div className='md:w-[13%] w-30 md:h-45 h-30 rounded-[50%] border-4 relative md:top-15 shrink-0 md:left-5 border-[#E8F5E9] bg-white'>
+            <div className='w-20 h-20 rounded-[50%] absolute bottom-0 right-0 bg-center bg-cover' style={{backgroundImage: `url(${camera})`}}>
+
+            </div>
+          </div>
+
+          {/*  profile basic info  */}
+          <div className='h-full w-[50%] shrink-0 flex flex-col items-center justify-center gap-2 pl-5'>
+
+            <h1 className='w-full h-fit md:text-4xl font-["Fredoka"] font-semibold text-[#285943]'>
+              Aqua
+            </h1>
+            <h2 className='w-full h-fit text-2xl font-["nunito"] font-semibold text-[#3f8a68]'>
+              Plants make life better
+            </h2>
+
+            <h2 className='w-full h-fit text-xl font-["nunito"] font-semibold text-[#3f8a68] flex items-center gap-2'>
+              <IoMailOpenOutline />
+              user@example.com
+            </h2>
+            <h2 className='w-full h-fit text-xl font-["nunito"] font-semibold text-[#3f8a68] flex items-center gap-2'>
+              <IoCalendarClearOutline />
+              30/03/2005
+            </h2>
 
           </div>
 
-          <h1 className='flex-col gap-0.5 rounded-3xl bg-white h-fit w-50 border text-2xl z-50 relative md:top-15 flex items-center justify-center font-semibold font-["nunito"] p-2'>
-            {userName}
-            <p className='text-sm font-mono font-normal'>
-              join: 2 August 2026
-            </p>
-          </h1>
-
         </div>
-
-        
 
       </div>
 
