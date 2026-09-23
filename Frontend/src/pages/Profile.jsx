dfead87ddaf1c2bd737ee87ca38661c1ba1db82a
K-Anant412 <anantkore412@gmail.com>
2026-09-23 16:51:39 +0530

    UI(Profile): Completing the profile header section.

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -17,25 +17,37 @@ const Profile = () => {
 
   const [click, setClick] = useState(false);
 
-  useEffect(() => {
-    const user = localStorage.getItem('user');
-    if(!user) return;
-    console.log(user);
-  }, [click])
-  
-
+  // useEffect(() => {
+  //   const user = localStorage.getItem('user');
+  //   if(!user) return;
+  //   console.log(user);
+  // }, [click])
 
   return (
-    <section className='w-full min-h-screen flex items-center justify-center bg-[#E8F5E9] md:p-5 p-3'>
-      <div className='w-full h-screen border-2 border-black'>
+    <section className='w-full min-h-screen bg-[#E8F5E9]'>
+      <div className='w-full h-screen border-2 border-black flex flex-col md:p-0 p-3 '>
 
-        <button
-          onClick={()=>setClick(!click)}
-          className='w-fit h-fit text-xl font-semibold py-3 px-4 border rounded-2xl cursor-pointer'
-        >
-          Click
-        </button>
+        {/*  Profile header  */}
+        <div className='relative w-full md:h-60 h-fit md:rounded-0 rounded-2xl bg-white flex md:flex-row flex-col items-center md:p-5 p-2.5'>
+          {/*  Profile Image  */}
+          <div className='shrink-0 md:w-40 w-25 md:h-40 h-25 border-2 rounded-[50%] bg-center bg-cover' style={{backgroundImage: `url(${profile})`}}>
 
+          </div>
+          {/*  User name, email, account date */}
+          <div className='shrink-0 md:w-full md:h-full relative md:top-0 -top-4 md:p-4 p-2 flex flex-col justify-center'>
+
+            <h1 className='w-full p-2 h-fit md:text-3xl text-[24px] font-["nunito"] font-extrabold flex items-center justify-center md:items-start md:justify-normal'>
+              User Name
+            </h1>
+            <h1 className='w-full pl-2 h-fit md:text-2xl text-xl font-["nunito"] flex items-center font-semibold'>
+              -anantkore412@gmail.com
+            </h1>
+            <h1 className='w-full pl-2 h-fit md:text-2xl text-xl font-["nunito"] flex items-center font-semibold'>
+              -12 Sep, 2026
+            </h1>
+
+          </div>
+        </div>
       </div>
     </section>
   )
